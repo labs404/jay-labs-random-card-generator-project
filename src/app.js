90c2269ca52cfabfa21cb8c012d8a3a94f51fa32
labs404 <136141267+labs404@users.noreply.github.com>
2023-07-18 01:08:01 +0000

    initial commit. needs styling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
 /* eslint-disable */
-import "bootstrap";
+// import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  // create arrays of suits and values
+  let suits = ["♣", "♠", "♦", "♥"];
+  let value = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+
+  // generate random suit and value
+  let randomSuit = () => {
+    return Math.floor(Math.random() * 4);
+  };
+  let randomValue = () => {
+    return Math.floor(Math.random() * 12);
+  };
+
+  // draw a card
+  let drawTheCard = () => {
+    document.querySelector("#theCard").innerHTML = `
+    <div id="cardSuitTop"></div>
+    <div id="cardValue"></div>
+    <div id="cardSuitBottom"></div>`;
+
+    let drawnSuit = suits[randomSuit()];
+    let drawnValue = value[randomValue()];
+    var suitColor = "blue";
+
+    if (drawnSuit === "♥" || drawnSuit === "♦") {
+      var suitColor = "red";
+    } else {
+      var suitColor = "black";
+    }
+    document.querySelector(
+      "#cardSuitTop"
+    ).innerHTML = `<div style="color:${suitColor}">${drawnSuit}</div>`;
+    document.querySelector(
+      "#cardValue"
+    ).innerHTML = `<div style="color:${suitColor}">${drawnValue}</div>`;
+    document.querySelector(
+      "#cardSuitBottom"
+    ).innerHTML = `<div style="color:${suitColor}">${drawnSuit}</div>`;
+  };
+  // drawTheCard();
+  document
+    .getElementById("documentButton")
+    .addEventListener("click", drawTheCard);
 };
